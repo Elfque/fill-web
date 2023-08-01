2954ebb8d885d1cd08e2b24b6f19adf816a1bf8c
Elfque <100863540+Elfque@users.noreply.github.com>
2023-08-01 07:59:11 +0100

    carousel for team

--- script.js
+++ script.js
@@ -16,3 +16,29 @@ $(".hold").slick({
   prevArrow: $(".prev"),
   nextArrow: $(".next"),
 });
+
+$(".pictures").slick({
+  // dots: true,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  centerMode: true,
+  prevArrow: $(".prev1"),
+  nextArrow: $(".next1"),
+  responsive: [
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
