02f201c027737739b278408c3fffa95c214c6159
Elfque <100863540+Elfque@users.noreply.github.com>
2023-08-01 20:34:36 +0100

    three

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const dropBtn = document.querySelectorAll(".drop_button");
+const navBtn = document.querySelector(".nav-button");
+const list = document.querySelector("nav ul");
+const close = document.querySelector(".close");
 
 dropBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -8,13 +11,29 @@ dropBtn.forEach((btn) => {
 });
 
 $(".hold").slick({
-  dots: true,
+  // dots: true,
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 3,
   centerMode: true,
   prevArrow: $(".prev"),
   nextArrow: $(".next"),
+  responsive: [
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 
 $(".pictures").slick({
@@ -41,4 +60,12 @@ $(".pictures").slick({
       },
     },
   ],
+});
+
+navBtn.addEventListener("click", () => {
+  list.classList.toggle("hide");
+});
+
+close.addEventListener("click", () => {
+  list.classList.toggle("hide");
 });
