b5114198e4265fc9fcddcec4b254048d195115a9
Elfque <100863540+Elfque@users.noreply.github.com>
2023-08-02 20:00:39 +0100

    smalls

--- script.js
+++ script.js
@@ -7,60 +7,14 @@ dropBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
     const parentDrop = btn.closest(".drop");
     parentDrop.classList.toggle("active");
+    if (parentDrop.classList.contains("active")) {
+      btn.textContent = "-";
+    } else {
+      btn.textContent = "+";
+    }
   });
 });
 
-// $(".hold").slick({
-//   // dots: true,
-//   infinite: true,
-//   slidesToShow: 3,
-//   slidesToScroll: 3,
-//   centerMode: true,
-//   prevArrow: $(".prev"),
-//   nextArrow: $(".next"),
-//   responsive: [
-//     {
-//       breakpoint: 1000,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 2,
-//       },
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//       },
-//     },
-//   ],
-// });
-
-// $(".pictures").slick({
-//   // dots: true,
-//   infinite: true,
-//   slidesToShow: 3,
-//   slidesToScroll: 3,
-//   centerMode: true,
-//   prevArrow: $(".prev1"),
-//   nextArrow: $(".next1"),
-//   responsive: [
-//     {
-//       breakpoint: 1000,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 2,
-//       },
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//       },
-//     },
-//   ],
-// });
 const swiper = new Swiper(".swiper", {
   speed: 1000,
   spaceBetween: 40,
